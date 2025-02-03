a683b1e955b0e61bb7d8f6d68ab457eb3c9b08a8
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-02-03 09:40:03 +0530

    ToolBar Fixed for responsive

--- src/app/pages/toolbar/toolbar.component.ts
+++ src/app/pages/toolbar/toolbar.component.ts
@@ -1,13 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-
 @Component({
   selector: 'app-toolbar',
   imports: [RouterModule, MatToolbarModule, 
-    MatButtonModule],
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
