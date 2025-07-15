d7b8586898af68de35969825ba0606decb9539f0
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-07-15 20:26:35 +0530

    using the cropper image

--- src/app/pages/member/member/member.component.ts
+++ src/app/pages/member/member/member.component.ts
@@ -22,10 +22,71 @@ import {MatInputModule} from '@angular/material/input';
   styleUrl: './member.component.scss'
 })
 export class MemberComponent {
+  previewAdharUrl: string | ArrayBuffer | null = null;
+imageAdharError: string | null = null;
+ previewReceiptUrl: string | ArrayBuffer | null = null;
+imageReceiptError: string | null = null;
+maxFileSizeMB = 2;
   constructor(public dialogRef: MatDialogRef<MemberComponent>) {}
 
   closeDialog(): void {
     this.dialogRef.close();
   }
+
+  onAdharSelected(event: Event): void {
+  const file = (event.target as HTMLInputElement).files?.[0];
+
+  this.imageAdharError = null;
+  this.previewAdharUrl = null;
+
+  if (file) {
+    const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+    const maxSize = this.maxFileSizeMB * 1024 * 1024;
+
+    if (!allowedTypes.includes(file.type)) {
+      this.imageAdharError = 'Only JPG and PNG images are allowed.';
+      return;
+    }
+
+    if (file.size > maxSize) {
+      this.imageAdharError = `File size should be less than ${this.maxFileSizeMB}MB.`;
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewAdharUrl = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+  onReceiptSelected(event: Event): void {
+  const file = (event.target as HTMLInputElement).files?.[0];
+
+  this.imageReceiptError = null;
+  this.previewReceiptUrl = null;
+
+  if (file) {
+    const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+    const maxSize = this.maxFileSizeMB * 1024 * 1024;
+
+    if (!allowedTypes.includes(file.type)) {
+      this.imageReceiptError = 'Only JPG and PNG images are allowed.';
+      return;
+    }
+
+    if (file.size > maxSize) {
+      this.imageReceiptError = `File size should be less than ${this.maxFileSizeMB}MB.`;
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewReceiptUrl = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
 
 }
