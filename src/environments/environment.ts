9289dea02b26921a4f706e7eca26c51511d90a84
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-07-16 12:48:12 +0530

    assing member service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,23 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://api.dev.example.com',
-    openaiApiKey: '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',
-    firebaseConfig : {
-      apiKey: "AIzaSyATbQrUqnDHHg3fKBoQ9yRkR_sLyNME-I0",
-      authDomain: "quizchamp-2ee33.firebaseapp.com",
-      projectId: "quizchamp-2ee33",
-      storageBucket: "quizchamp-2ee33.firebasestorage.app",
-      messagingSenderId: "719485353665",
-      appId: "1:719485353665:web:34aba1265b0315a97ede40"
-    }
-  };
+  production: false,
 
-  // Import the functions you need from the SDKs you need
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHPRQVZdSkxcKg6S9Fc_ZNCG52V5G_UdM',
+    authDomain: 'brothers-helping-group.firebaseapp.com',
+    projectId: 'brothers-helping-group',
+    storageBucket: 'brothers-helping-group.firebasestorage.app',
+    messagingSenderId: '242087385547',
+    appId: '1:242087385547:web:9dfeacfd700984620c3e4e',
+    measurementId: 'G-N59BW6E01X',
+  },
+};
+
+// Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
-// const 
+// const
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
