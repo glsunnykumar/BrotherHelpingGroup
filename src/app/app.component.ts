ca6f9d59aa51eed26bad022fdd9b687bc7a8d30a
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-07-26 16:28:02 +0530

    aDDING THE WHATS APP BUTTON AND FOOTER GOLABALLY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from "./pages/toolbar/toolbar.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ ToolbarComponent],
+  imports: [ ToolbarComponent,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
