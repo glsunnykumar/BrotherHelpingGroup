57ed220095e10e9aa4b0e0e81699db7890d067a7
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-07-30 10:46:31 +0530

    added vistor count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, doc, getDoc, updateDoc, increment } from '@angular/fire/firestore';
 import { ToolbarComponent } from "./pages/toolbar/toolbar.component";
 import { MatIconModule } from '@angular/material/icon';
 
@@ -12,4 +13,21 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class AppComponent {
   title = 'brotherhelpinggroup';
-}
+   visitorCount: number = 0;
+  constructor(private firestore: Firestore) {}
+
+    async ngOnInit() {
+    const docRef = doc(this.firestore, 'analytics/visits');
+    await updateDoc(docRef, {
+      count: increment(1)
+    });
+
+    const docRef1 = doc(this.firestore, 'analytics/visits');
+    const snapshot = await getDoc(docRef1);
+    if (snapshot.exists()) {
+      this.visitorCount = snapshot.data()['count'] || 0;
+    }
+  }
+  }
+
+
