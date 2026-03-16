3045ecea6d340b277210273016f4b4f2512c7005
glsunnykumar <glsunny.kumar@gmail.com>
2026-03-16 10:10:24 +0530

    Adding New Functionality to the admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,21 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api.example.com',
-    openaiApiKey: 'your-prod-openai-api-key',
-  };
+  production: false,
+
+  firebaseConfig: {
+    //   apiKey: "AIzaSyCTes-lcaxTAnx8UO4nFGfFTVSh_EchqEY",
+    // authDomain: "tour-travel-app-904ef.firebaseapp.com",
+    // projectId: "tour-travel-app-904ef",
+    // storageBucket: "tour-travel-app-904ef.firebasestorage.app",
+    // messagingSenderId: "545224972452",
+    // appId: "1:545224972452:web:98c98ad805514b5acd2e31",
+    // measurementId: "G-L8VMDE66DQ"
+
+    apiKey: 'AIzaSyCHPRQVZdSkxcKg6S9Fc_ZNCG52V5G_UdM',
+    authDomain: 'brothers-helping-group.firebaseapp.com',
+    projectId: 'brothers-helping-group',
+    storageBucket: 'brothers-helping-group.firebasestorage.app',
+    messagingSenderId: '242087385547',
+    appId: '1:242087385547:web:9dfeacfd700984620c3e4e',
+    measurementId: 'G-N59BW6E01X',
+  },
+};
