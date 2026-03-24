4a32f6480f29df1d45104e6647a9d91d2538f589
glsunnykumar <glsunny.kumar@gmail.com>
2026-03-24 13:44:39 +0530

    Before Working on the login Page of the Admin Panel

--- src/app/pages/blog-detail/blog-detail.component.ts
+++ src/app/pages/blog-detail/blog-detail.component.ts
@@ -3,58 +3,57 @@ import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../../admin/blog/blog.service';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { GlobalLoaderComponent } from '../../shared/global-loader/global-loader.component';
 
 @Component({
   selector: 'app-blog-detail',
-  imports: [CommonModule ,
-    MatIconModule
-  ],
+  imports: [CommonModule, MatIconModule, GlobalLoaderComponent],
   templateUrl: './blog-detail.component.html',
-  styleUrl: './blog-detail.component.scss'
+  styleUrl: './blog-detail.component.scss',
 })
 export class BlogDetailComponent {
-
   private route = inject(ActivatedRoute);
   private blogService = inject(BlogService);
+  isLoading = false;
 
   readingTime: number = 0;
-relatedBlogs: any[] = [];
+  relatedBlogs: any[] = [];
   blog: any;
 
-  async ngOnInit() {
-
+  async ngOnInit() 
+  {
+    this.isLoading = true;
     const id = this.route.snapshot.paramMap.get('id');
-
+    
     if (id) {
       this.blog = await this.blogService.getBlogById(id);
-        this.calculateReadingTime(this.blog.content);
+      this.calculateReadingTime(this.blog.content);
       // Fetch related blogs
-    this.blogService.getBlogs().subscribe(res => {
-      this.relatedBlogs = res.filter(b => b.id !== id).slice(0, 3);
-    });
+      this.blogService.getBlogs().subscribe((res) => {
+        this.relatedBlogs = res.filter((b) => b.id !== id).slice(0, 3);
+        this.isLoading = false;
+      });
     }
-
   }
 
   calculateReadingTime(text: string) {
-  const words = text.split(' ').length;
-  this.readingTime = Math.ceil(words / 200); // 200 words/min
-}
-
-share(platform: string){
-
-  const url = window.location.href;
-  console.log(url);
-
-  if(platform === 'whatsapp'){
-    window.open(`https://wa.me/?text=${url}`);
+    const words = text.split(' ').length;
+    this.readingTime = Math.ceil(words / 200); // 200 words/min
   }
 
-  if(platform === 'facebook'){
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, '_blank');
+  share(platform: string) {
+    const url = window.location.href;
+    console.log(url);
+
+    if (platform === 'whatsapp') {
+      window.open(`https://wa.me/?text=${url}`);
+    }
+
+    if (platform === 'facebook') {
+      window.open(
+        `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+        '_blank',
+      );
+    }
   }
-
-}
-
-
 }
--- src/app/pages/blog-list/blog-list.component.ts
+++ src/app/pages/blog-list/blog-list.component.ts
@@ -2,30 +2,37 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { BlogService } from '../../admin/blog/blog.service';
+import { GlobalLoaderComponent } from '../../shared/global-loader/global-loader.component';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [CommonModule],
+  imports: [CommonModule, GlobalLoaderComponent],
   templateUrl: './blog-list.component.html',
-  styleUrl: './blog-list.component.scss'
+  styleUrl: './blog-list.component.scss',
 })
 export class BlogListComponent {
-
-   private blogService = inject(BlogService);
+  private blogService = inject(BlogService);
   private router = inject(Router);
 
   blogs: any[] = [];
+  isLoading = false;
 
   ngOnInit() {
+    this.isLoading = true;
 
-    this.blogService.getBlogs().subscribe(res => {
-      this.blogs = res;
+    this.blogService.getBlogs().subscribe({
+      next: (res) => {
+        this.blogs = res;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching blogs:', error);
+        this.isLoading = false;
+      },
     });
-
   }
 
   openBlog(id: string) {
     this.router.navigate(['/blog', id]);
   }
-
 }
